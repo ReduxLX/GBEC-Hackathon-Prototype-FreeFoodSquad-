b663447a3589264b1ecef9ce51e5ad94cc1f94ea
jackywcyong <45890899+jackywcyong@users.noreply.github.com>
2018-12-16 05:01:53 +0800

    Add files via upload

---
+++ GBEC_project/Tax.sol
@@ -0,0 +1,70 @@
+pragma solidity ^0.4.25;
+
+contract GsDynamic{
+    uint public frequency;
+    uint public transaction;
+    uint public tax;
+    address public buyerAddress;
+    uint public dayCounter = 0;
+
+    struct Person{
+        int idCardNumber;
+        uint purchaseNumber;
+    }
+
+    mapping (address => Person) public persons;
+
+    constructor(uint intialtransaction,address initialAddress) public{
+        transaction = intialtransaction;
+        buyerAddress = initialAddress;
+        calculateTax();
+    }
+
+    function addPerson(address _address, int _idCardNumber, uint _purchaseNumber)public{
+        persons[_address] = Person(_idCardNumber, _purchaseNumber);
+    }
+
+    function setBuyerAddress(address _buyerAddress) public{
+        buyerAddress = _buyerAddress;
+        calculateTax();
+    }
+
+    function calculateTax() private{
+        if (persons[buyerAddress].idCardNumber != 0){
+            frequency = persons[buyerAddress].purchaseNumber;
+            if(dayCounter>31){
+                dayCounter = 0;
+                frequency = 0;
+            }
+            frequency++;
+            dayCounter++;
+            tax = transaction/20; //5% Base Tax
+            if(frequency>10){
+                tax = transaction/10; //10%
+            }
+            if(frequency>20){
+                tax = transaction/5; //20%
+            }
+            persons[buyerAddress].purchaseNumber = frequency;
+        }
+        else{
+            if(dayCounter>31){
+                dayCounter = 0;
+                frequency = 0;
+            }
+            else if (dayCounter>=0 && dayCounter<=31){
+            frequency = 1;//the first time purchase
+            tax = transaction/20;
+            dayCounter++;
+            addPerson(buyerAddress, 16122018, frequency);
+            }
+        }
+
+    }
+    function simulateTransaction(uint nextTransaction) public{
+        transaction = nextTransaction;
+    }
+    function advanceTenDays() public{
+        dayCounter += 10;
+    }
+}
